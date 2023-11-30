da4787822752d59201585ead9bc8d8d9152a507f
Melek Deniz Colak <deniz@Meleks-MacBook-Air.local>
2023-11-29 21:00:05 -0500

    added state_of_game and end_game

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -6,6 +6,7 @@ import numpy as np
 from copy import deepcopy
 import time
 import heapq
+import logging
 
 
 max_depth = 3
@@ -156,6 +157,85 @@ class StudentAgent(Agent):
         # Set the opposite barrier to True
         move = self.moves[dir]
         chess_board[r + move[0], c + move[1], self.opposites[dir]] = True
+
+     def endgame(self, chess_board, my_pos, adv_pos):
+        board_size = chess_board.shape[0]
+        r,c = my_pos
+        # Union-Find
+        parent = dict()
+        for r in range(board_size):
+            for c in range(board_size):
+                # self loop: initially, each element points to itseldf
+                parent[(r,c)]= (r,c)
+
+        def union(pos1, pos2):
+            parent[pos1]= pos2
+
+        # follow parent node until you reach a self loop
+        def find(pos):
+            if parent[pos] != pos:
+                parent[pos] = find(parent[pos])
+            return parent[pos]
+
+        for r in range(board_size):
+            for c in range(board_size):
+                for dir, move in enumerate(
+                        self.moves[1:3]
+                ):  # Only check down and right
+                    if self.chess_board[r, c, dir + 1]:
+                        continue
+                    pos_a = find((r, c))
+                    pos_b = find((r + move[0], c + move[1]))
+                    if pos_a != pos_b:
+                        union(pos_a, pos_b)
+
+        for r in range(self.board_size):
+            for c in range(self.board_size):
+                find((r, c))
+        my_pos_r = find(tuple(my_pos))
+        adv_pos_r = find(tuple(adv_pos))
+        my_pos_score = list(parent.values()).count(my_pos_r)
+        adv_pos_score = list(parent.values()).count(adv_pos_r)
+        if my_pos_r == adv_pos_r:
+            return False, my_pos_score, adv_pos_score
+        player_win = None
+        win_blocks = -1
+        if my_pos_score > adv_pos_score:
+            player_win = 0
+            win_blocks = my_pos_score
+        elif my_pos_score < adv_pos_score:
+            player_win = 1
+            win_blocks = adv_pos_score
+        else:
+            player_win = -1  # Tie
+        if player_win >= 0:
+            logging.info(
+                f"Game ends! Player {self.player_names[player_win]} wins having control over {win_blocks} blocks!"
+            )
+        else:
+            logging.info("Game ends! It is a Tie!")
+        return True, my_pos_score, adv_pos_score
+    
+    def state_of_game(chess_board):
+        remaining_pos = []
+        board_size = chess_board.shape[0]
+        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+
+        for i in range(board_size):
+            for j in range(board_size):
+                remaining_pos.extend([
+                    d for d in range(4)  # 4 moves possible
+                    if not chess_board[i, j, d]  # chess_board True means wall
+                ])
+        cur_count = len(remaining_pos)
+        initial_count = (4 * board_size * board_size) - (4 * board_size)
+
+        if cur_count / initial_count >= 0.4:
+            return 1 #beginning game
+        elif 0.4 < cur_count / initial_count <= 0.6:
+            return 2 #middle game
+        else:
+            return 3 #end game
 
     def eval_function(self):
         self.get_available_move_number()
